4f41a02cfb5a5b2f2259ee97c67dc8fc2ffe16b9
Jingyu Qi <cs1jqi@sheffield.ac.uk>
2023-01-25 00:17:10 +0000

    init

---
+++ spring-boot-gethub-test/src/main/java/its/shef/ac/uk/github/test/SpringBootGethubTestApplication.java
@@ -0,0 +1,13 @@
+package its.shef.ac.uk.github.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGethubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGethubTestApplication.class, args);
+	}
+
+}
---
+++ spring-boot-gethub-test/src/test/java/its/shef/ac/uk/github/test/SpringBootGethubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package its.shef.ac.uk.github.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGethubTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
